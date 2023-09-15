59f88896cd86484393497acd0ccaa272f746a568
Rudyboy <rudylepretre@gmail.com>
2023-09-15 19:24:35 +0200

    First scope of onBoardingScreen

--- app/src/main/java/com/example/trainingcomposeandcleanarchitecture/MainActivity.kt
+++ app/src/main/java/com/example/trainingcomposeandcleanarchitecture/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.trainingcomposeandcleanarchitecture
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.trainingcomposeandcleanarchitecture.presentation.onboarding.OnBoardingScreen
 import com.example.trainingcomposeandcleanarchitecture.ui.theme.TrainingComposeandCleanArchitectureTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,30 +22,10 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         setContent {
             TrainingComposeandCleanArchitectureTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
+                    OnBoardingScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TrainingComposeandCleanArchitectureTheme {
-        Greeting("Android")
     }
 }
