a98f48b5f1ca23763accb31c6e2b08f9d8a46485
Rudyboy <rudylepretre@gmail.com>
2023-09-16 10:40:41 +0200

    Implemment datastore preferences

---
+++ app/src/main/java/com/example/trainingcomposeandcleanarchitecture/domain/usecases/ReadAppEntry.kt
@@ -0,0 +1,12 @@
+package com.example.trainingcomposeandcleanarchitecture.domain.usecases
+
+import com.example.trainingcomposeandcleanarchitecture.domain.manger.LocalUserManger
+import kotlinx.coroutines.flow.Flow
+
+class ReadAppEntry(
+    private val localUserManger: LocalUserManger
+) {
+    suspend operator fun invoke(): Flow<Boolean> {
+        return localUserManger.readAppEntry()
+    }
+}
