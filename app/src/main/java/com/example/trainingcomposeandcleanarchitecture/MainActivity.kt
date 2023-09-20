489da1a5b0f7b9eabd1ee1cbffd3c5ec1828b26a
Rudyboy <rudylepretre@gmail.com>
2023-09-20 18:09:22 +0200

    Add system ui controller

--- app/src/main/java/com/example/trainingcomposeandcleanarchitecture/MainActivity.kt
+++ app/src/main/java/com/example/trainingcomposeandcleanarchitecture/MainActivity.kt
@@ -6,13 +6,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
@@ -25,6 +28,7 @@ import com.example.trainingcomposeandcleanarchitecture.presentation.navgraph.Nav
 import com.example.trainingcomposeandcleanarchitecture.presentation.onboarding.OnBoardingScreen
 import com.example.trainingcomposeandcleanarchitecture.presentation.onboarding.OnBoardingViewModel
 import com.example.trainingcomposeandcleanarchitecture.ui.theme.TrainingComposeandCleanArchitectureTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -36,7 +40,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)//todo marche pas
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         installSplashScreen().apply {
             setKeepOnScreenCondition(condition = { viewModel.splashCondition.value })
 
@@ -44,7 +48,19 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             TrainingComposeandCleanArchitectureTheme {
-                Box(modifier = Modifier.background(MaterialTheme.colorScheme.background).fillMaxSize()) {
+                val isSystemInDarkMode = isSystemInDarkTheme()
+                val systemController = rememberSystemUiController()
+
+                SideEffect {
+                    systemController.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons = !isSystemInDarkMode
+                    )
+                }
+
+                Box(modifier = Modifier
+                    .background(MaterialTheme.colorScheme.background)
+                    .fillMaxSize()) {
                     NavGraph(startDestination = viewModel.startDestination.value)
                 }
             }
